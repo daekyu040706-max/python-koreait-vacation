e1b1c86606e1579463761fe22dcb629c5dbf1128
daekyu040706 <daekyu040706@gmail.com>
2026-02-23 11:40:37 +0900

    requests 끝

---
+++ day19/셀레니움01.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
